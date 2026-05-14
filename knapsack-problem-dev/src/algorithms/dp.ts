b4198a4f104bc97bdd8e7a7d652cbeb3f3db3e14
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-05-14 13:31:18 +0300

    create: dp algorithm

---
+++ knapsack-problem-dev/src/algorithms/dp.ts
@@ -0,0 +1,59 @@
+import type { IStep } from "../types/IStep";
+
+export default function knapsack(weights: number[], values: number[], W: number) {
+  // start time
+  const start = performance.now();
+  
+  const n = weights.length;
+  const dp = Array.from({ length: n + 1 }, () =>
+    Array(W + 1).fill(0)
+  );
+  const steps: IStep[] = [];   
+
+  for (let i = 1; i <= n; i++) {
+    for (let w = 0; w <= W; w++) {
+      const weight = weights[i - 1];
+      const value = values[i - 1];
+
+      let newValue = dp[i - 1][w]
+
+      if (weight <= w) {
+        newValue = Math.max(
+          dp[i - 1][w],                  
+          dp[i - 1][w - weight] + value 
+        )
+      }
+      dp[i][w] = newValue;
+      
+      steps.push({
+        i,
+        w,
+        value: newValue,
+        dpCurrState: structuredClone(dp)  
+      })
+      
+    }
+  }
+  const end = performance.now();
+  const time = end - start;
+  
+  console.log("result: ", dp);
+
+  return {dp, steps, time};
+}
+
+export const getItems = (dp: number[][], weights: number[], W: number) => {
+  let i = weights.length;
+  let w = W;
+  const items = [];
+
+  while (i > 0 && w > 0) {
+    if (dp[i][w] !== dp[i - 1][w]) {
+      items.push(i); 
+      w -= weights[i - 1];
+    }
+    i--;
+  }
+
+  return items.reverse();
+}
