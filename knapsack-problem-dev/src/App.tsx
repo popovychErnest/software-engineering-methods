566647fe182251251f2953cb0837adb4b86b93e6
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-05-18 23:07:26 +0300

    refactoring, replace dp logic in DP compo

--- knapsack-problem-dev/src/App.tsx
+++ knapsack-problem-dev/src/App.tsx
@@ -3,104 +3,107 @@ import { Input } from "./Input";
 import DynamicProgramming from "./dynamic-programming/DynamicProgramming";
 import { AnimatePresence } from "framer-motion";
 import { useAlgorithms } from "./hooks/useAlgorithms";
-import type { IStep } from "./types/IStep";
-import type { AlgorithmTypes } from "./types/IAlgorithms";
+import type { AlgorithmName, TAlgorithmStatus } from "./types/IAlgorithms";
 
 import {motion} from "framer-motion";
+import BruteForce from "./brute-force/BruteForce";
 
 function App() {
   const boundsRef = useRef<HTMLElement>(null);
-  const { activeAlgorithms, setActiveAlgorithms } = useAlgorithms();
+  const { algorithms, setAlgorithms } = useAlgorithms();
+  const [ activeAlgorithms, setActiveAlgorithms ] = useState<Record<AlgorithmName, boolean>>({dp: false, bruteforce: false});
+  const [currentSteps, setCurrentSteps] = useState<Record<AlgorithmName, number>>({dp: 0, bruteforce: 0})
 
-
+  // INPLEMENT USESTATE STATUS, SETSTATUS!!! (refactor, mb replace activeAlgorithms)
+  const [status, setStatus] = useState<Record<AlgorithmName, TAlgorithmStatus>>({dp: "unavailable", bruteforce: "unavailable"});
+  
   const [weights, setWeights] = useState<number[]>([]);
   const [values, setValues] = useState<number[]>([]);
   const [knapsackWeight, setKnapsackWeight] = useState<number>(0);
 
-  const [steps, setSteps] = useState<IStep[]>([]);
-  const [currentStep, setCurrentStep] = useState<number>(0);
 
-  const pauseAlgorithms = (param: "all" | AlgorithmTypes ): void => {
+  const controlAlgorithms = (status: TAlgorithmStatus, param: "all" | AlgorithmName = "all" ): void => {
     switch (param) {
-      case "all": stopAlgorithms(); return;
-      case "dp": setActiveAlgorithms(prev => ({...prev, dp: false}));  return;
+      case "all": runAlgorithms(status); return;
+      case "dp": setStatus(prev => ({...prev, dp: status})); return;
+      case "bruteforce": setStatus(prev => ({...prev, bruteforce: status})); return;
     }
   }
 
-  const stopAlgorithms = () => {
-    setActiveAlgorithms((prev) => {
+
+  const runAlgorithms = (status: TAlgorithmStatus) => {
+    setStatus((prev) => {
       const updated = { ...prev };
-      (Object.keys(updated) as AlgorithmTypes[]).forEach((key) => {
-        updated[key] = false;
+      (Object.keys(updated) as AlgorithmName[]).forEach((k) => {
+        updated[k] = status;
       });
       return updated;
     });
   };
-
-  const visitedDp =
-    steps[currentStep]?.dpCurrState ??
-    Array.from({ length: 3 }, () => Array(3).fill(0));
-
-  useEffect(() => {
-    if (currentStep === steps?.length - 1) {
-      // setCurrentStep(0);
-      stopAlgorithms();
-      return;
-    }
-    // if all algorithms stopped
-    if (Object.values(activeAlgorithms).every((el) => el == false)) return;
-
-    if (!weights.length && !values.length) return;
-
-    const timer = setTimeout(() => {
-      console.log(steps);
-      setCurrentStep((prev) => prev + 1);
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, [steps, currentStep]);
-
-  useEffect(() => {
-    // alert(JSON.stringify(activeAlgorithms));
-    // alert(JSON.stringify(steps));
-  }, [activeAlgorithms, steps]);
-
-  
-
+  // useEffect(() => {alert("CURRENT STATUSES: "+JSON.stringify(status))}, [status])
   return (
     <>
       <AnimatePresence mode="wait">
         <main  className=" w-screen  bg-neutral-800 box-border py-10 h-screen space-y-10  flex flex-col justify-center ">
             <Input
-              isAlgoRunning={Object.values(activeAlgorithms).some(el => el === true)}
+              isAlgosRunning={(Object.values(status) as TAlgorithmStatus[]).some(el => el === "running")}
+              isAlgosPaused={(Object.values(status) as TAlgorithmStatus[]).some(el => el === "paused")}
+
+              // isNoOneIsRunning = {Object.values(activeAlgorithms).every(el => el === false)}
               setWeights={setWeights}
               setValues={setValues}
               setKnapsackWeight={setKnapsackWeight}
-              setSteps={setSteps}
-              currentStep={currentStep}
-              pauseAlgorithms={pauseAlgorithms}
-              handleResetStep={() => setCurrentStep(0)}
-              bounds={boundsRef}
-    
+              
+              setCurrentSteps={setCurrentSteps}
+              controlAlgorithms = {controlAlgorithms}
+
+              bounds={boundsRef} 
               />
           {/* <motion.section className="border-red-500  flex justify-center relative rounded-2xl px-10 mx-10 w-max-full h-full"> */}
-        <motion.section ref= {boundsRef} className="w-[calc(100%-1rem)] z-1 absolute p-4 border border-red-500 left-4 top-4 h-[calc(100%-1rem)] "></motion.section>
-            {weights.length && values.length && knapsackWeight !== 0 ? (
-              
-              <DynamicProgramming
-              values={values}
-              pauseAlgorithms={pauseAlgorithms}
-              bounds = {boundsRef}
-              weights={weights}
-              knapsackWeight={knapsackWeight}
-              dp={visitedDp}
-              currentStep={currentStep} 
-              steps={steps}
-              algoFinished = {steps.length-1 === currentStep}
-                />
-            ) : (
-              <h1 className="text-white text-4xl">Still empty...</h1>
-            )}
+        <motion.section ref= {boundsRef} className="w-[calc(100%-2rem)] z-1 absolute rounded-2xl p-4 border border-neutral-500 left-4 top-4 h-[calc(100%-2rem)] "></motion.section>
+            {/* {Object.keys(activeAlgorithms).map(k => { */}
+              {/* activeAlgorithms[k] &&  */}
+            {/* })} */}
+
+              {status.dp !== "unavailable" && 
+               <DynamicProgramming
+               type = {"dp"}
+
+               values={values}
+               weights={weights}
+               knapsackWeight={knapsackWeight}
+
+               status ={status.dp}
+               currentStep = {currentSteps.dp}
+               algorithm = {algorithms.dp}
+               
+               setCurrentSteps = {setCurrentSteps}
+               controlAlgorithm={controlAlgorithms}
+
+               bounds = {boundsRef}/>
+              }
+
+              {status.bruteforce !== "unavailable" &&
+              <BruteForce
+              type = {"bruteforce"}
+
+               values={values}
+               weights={weights}
+               knapsackWeight={knapsackWeight}
+
+               status ={status.bruteforce}
+               currentStep = {currentSteps.bruteforce}
+               algorithm = {algorithms.bruteforce}
+               
+               setCurrentSteps = {setCurrentSteps}
+               controlAlgorithm={controlAlgorithms}
+
+               bounds = {boundsRef}/>
+              }
+
+            {/* ) : ( */}
+              {/* <h1 className="text-white absolute bottom-2 left-2 text-4xl ">Still empty...</h1> */}
+            
           {/* </motion.section> */}
         </main>
       </AnimatePresence>
