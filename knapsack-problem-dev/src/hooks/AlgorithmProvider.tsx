22f75f908e77f8c417d2312dffae187e8ea8d1d0
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-05-18 23:09:59 +0300

    refactor, add 'bruteforce' algo

--- knapsack-problem-dev/src/hooks/AlgorithmProvider.tsx
+++ knapsack-problem-dev/src/hooks/AlgorithmProvider.tsx
@@ -1,18 +1,20 @@
 import { createContext, useState, type Dispatch, type ReactNode, type SetStateAction } from "react";
 import { type IAlgorithms } from "../types/IAlgorithms";
+import { steps } from "framer-motion";
 
 type AlgorithmContextType = {
-    activeAlgorithms: IAlgorithms,
-    setActiveAlgorithms: Dispatch<SetStateAction<IAlgorithms>>; 
+    algorithms: IAlgorithms,
+    setAlgorithms: Dispatch<SetStateAction<IAlgorithms>>; 
 }
 
 export const AlgorithmsContext = createContext<AlgorithmContextType | null>(null)
 
 export const AlgorithmsProvider = ({children}: {children: ReactNode}) => {
-     const [activeAlgorithms, setActiveAlgorithms] = useState<IAlgorithms>({dp: false});
+     const [algorithms, setAlgorithms] = useState<IAlgorithms>(
+        {dp:{steps: [], time: 0}, bruteforce:{maxValue: 0, time: 0, steps:[]}});
 
     return (
-    <AlgorithmsContext.Provider value={{activeAlgorithms, setActiveAlgorithms}}>
+    <AlgorithmsContext.Provider value={{algorithms, setAlgorithms}}>
         {children}
     </AlgorithmsContext.Provider>
     )
