b659c9d7a39c80e5d11022f2809050d2c902d1f4
Ernest-Projects <ernestpopovic5621@gmail.com>
2026-05-18 23:11:06 +0300

    large refactoring

--- knapsack-problem-dev/src/dynamic-programming/DynamicProgramming.tsx
+++ knapsack-problem-dev/src/dynamic-programming/DynamicProgramming.tsx
@@ -1,51 +1,88 @@
-import { useEffect, useRef, useState } from "react";
-import type { IStep } from "../types/IStep";
+import { useEffect, useRef, useState, type Dispatch, type SetStateAction } from "react";
 import { delay, motion, scale } from "framer-motion";
-import StopALgoButton from "../components/StopAlgoButton";
-import type { AlgorithmTypes } from "../types/IAlgorithms";
+import type { AlgorithmName, TAlgorithmStatus, DpAlgorithm } from "../types/IAlgorithms";
+import { animation, handleTextAppear } from "../helpers/AlgorithmShakeAnimation";
+import RunAlgoButton from "../components/RunAlgoButton";
 
 interface IDynamicProgramming {
   weights: number[];
   values: number[];
-
   knapsackWeight: number;
-  dp: number[][];
+  
+  algorithm: DpAlgorithm;
+  
   currentStep: number;
-  steps: IStep[];
+  setCurrentSteps: Dispatch<SetStateAction<Record<AlgorithmName, number>>>;
+
+  controlAlgorithm: (state: TAlgorithmStatus, param: "all" | AlgorithmName) => void;
+  
+  type: AlgorithmName;
+  status: TAlgorithmStatus;
+
   bounds: React.RefObject<HTMLElement | null>;
-  algoFinished: boolean,
-  pauseAlgorithms: (val: "all" | AlgorithmTypes) => void;
 }
 
 function DynamicProgramming({
+  
+  type,
+  
   weights,
   values,
-
   knapsackWeight,
-  dp,
+
+  algorithm,
   currentStep,
-  steps,
+  setCurrentSteps,
+
+  status,
+
+  controlAlgorithm,
+
   bounds,
-  algoFinished,
-  pauseAlgorithms
-}: IDynamicProgramming) {
+}: IDynamicProgramming) { 
   const windowRef = useRef<HTMLDivElement>(null);
   const [isClosed, setIsClosed] = useState<boolean>(false);
 
-  const step = steps[currentStep];
-  
+  const step = algorithm.steps[currentStep];
+  const steps = algorithm.steps;
+
+
+  const dp = algorithm.steps[currentStep].dpCurrState;
+  const time = algorithm.time;
+
+  const algoFinished = steps.length-1 === currentStep;
+
   // if less than next - skip
   const skipValue = dp[step.i-1][step.w];
 
+  // if bigger than prev - take
   const takeValue = step.w >= weights[step.i - 1]
     ? (dp[step.i - 1]?.[step.w - weights[step.i - 1]] ?? 0) + values[step.i - 1]
     : null;
 
-    const isAddedBigger = !!takeValue == false ? false :  takeValue > skipValue ? true : takeValue === skipValue ? false : false
-  useEffect(() => {
+    const isNewValTaken = !!takeValue == false ? false :  takeValue > skipValue ? true : takeValue === skipValue ? false : false
+  
+  
+    useEffect(() => {
     // alert(windowRef.current?.offsetWidth);
     currentStep == 0 && setIsClosed(false) 
   }, [currentStep])
+
+
+ useEffect(() => {
+    if (algoFinished) {
+      controlAlgorithm("finished", type)
+      return;
+    }
+
+    if(status !=="running") return;
+
+    const timer = setTimeout(() => {
+      setCurrentSteps(prev => ({...prev, dp: currentStep + 1}));
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, [steps, currentStep, status]);
 
 
   // useEffect(() => {
@@ -57,37 +94,7 @@ function DynamicProgramming({
   //   }
   // }, [currentStep])
 
-  const animation = {
-  init: {
-    rotate: 0,
-    x: 0,
-  },
 
-  shake: {
-    rotate: [-5, 5, -2, 2, 0],
-
-    transition: {
-      duration: 0.4,
-    },
-  },
-};
-
-  const text_appear = {
-  hidden: {
-    opacity: 0,
-    y: 0,
-    x: windowRef.current?.offsetWidth,
-  },
-
-  visible: {
-    x: windowRef.current?.offsetWidth,
-    y: -100,
-    opacity: [1,0],
-    transition: {
-      duration: .6,
-    },
-  },
-};
 
   return (
     <>
@@ -96,49 +103,45 @@ function DynamicProgramming({
       <motion.div
         ref={windowRef}
         drag
+        whileDrag={{scale: 1.05, boxShadow: "0px 0px 10px 1px black"}}
         dragConstraints={bounds}
         layout
         whileHover="visible"
         initial="hidden"
         variants={animation}
-        
+
         animate={algoFinished ? "shake" : "init"}
         exit={{ opacity: 0 }}
         
-        className="flex w-fit group/result relative z-40 top-0   flex-col h-fit  bg-neutral-700 border-neutral-500 rounded-2xl border py-4 pt-8 px-6 gap-2"
+        className="flex w-fit group/result relative z-40 top-0 group/appear  flex-col h-fit  bg-neutral-700 border-neutral-500 rounded-2xl border py-4 pt-8 px-6 gap-2"
       >
-        <motion.header variants={text_appear} animate = {algoFinished ? "visible": "hidden"} className="text-white opacity-0 text-2xl">Solved!</motion.header>
+        {status =="paused" && <motion.header animate= {{opacity: [1, 0.5, 0, 0.5, 1]}} transition={{duration: .5, repeat: Infinity, ease: "anticipate"}}  className="text-neutral-200 top-8 right-4 absolute text-2xl">Paused...</motion.header>}
+        {status =="finished" && <motion.header animate= {{opacity: [1, 0.5, 0, 0.5, 1]}} transition={{duration: .5, repeat: Infinity, ease: "anticipate"}}  className="text-green-300 top-8 right-4 absolute text-2xl">Finished!</motion.header>}
+        {status =="running" && <motion.header animate= {{opacity: [1, 0.5, 0, 0.5, 1]}} transition={{duration: .5, repeat: Infinity, ease: "anticipate"}}  className="text-yellow-200 top-8 right-4 absolute text-2xl">Running...</motion.header>}
+
+
+        <motion.header variants={handleTextAppear(windowRef)} animate = {algoFinished ? "visible": "hidden"} className="text-white opacity-0 text-2xl">Solved!</motion.header>
         {/* {algoFinished &&  */}
         <motion.div
-           transition={{ type: "spring", duration: .1, ease: "easeInOut" }}
-          variants={{
-            hidden: {
-              x: !windowRef.current?.offsetWidth
-                ? 100
-                :(windowRef.current.offsetWidth - 25),
-                // algoFinished ? (windowRef.current.offsetWidth - 25) 
+           transition={{ type: "spring", duration: .3, ease: "easeInOut" }}
+          //  animate={{ opacity: !algoFinished ? [1,0] : 1}}
 
-              opacity: 0
-            },
-            visible: { x: !windowRef.current?.offsetWidth
-                ? 100
-                : windowRef.current.offsetWidth - 25,
-              opacity: 1
-              },
-          }}
-          className={` z-[-1] h-fit text-white absolute transition gap-4 bg-neutral-700 rounded-tr-2xl rounded-br-2xl border  border-neutral-500 bottom-4 py-6 pl-8 pr-4 flex flex-col`}
+          layout
+          className={` z-[-1] h-fit text-white absolute left-full group-hover/appear:opacity-100 opacity-0 transition space-y-4 bg-neutral-700 rounded-tr-2xl rounded-br-2xl border  border-neutral-500  bottom-4 py-6 pl-4  pr-4 flex flex-col`}
         >
           {algoFinished ? 
           <>
           <header className="text-green-300 text-2xl">Result: </header>
-          <p>dp[{step.i}][{step.w}] = {dp[step.i][step.w]}</p>
+          <p className="whitespace-nowrap ml-4">dp[{step.i}][{step.w}] = {dp[step.i][step.w]}</p>
+
+          <header className="text-blue-300 mt-4 text-2xl">Time: </header>
+          <p className="text-2xl ml-4">{time}ms</p>
           </>
             :
           <>
           <>
-          <StopALgoButton handlePauseAlgorithms={() => pauseAlgorithms("dp")}></StopALgoButton>
             <header className="text-green-300 text-2xl">Current: </header>
-          <p>dp[{step.i}][{step.w}] = {dp[step.i][step.w]}</p>
+          <p className="ml-4">dp[{step.i}][{step.w}] = {dp[step.i][step.w]}</p>
           </>
           {/* <p>dp[{step.i}][{step.w}] = {dp[step.i][step.w]}</p> */}
 
@@ -146,21 +149,31 @@ function DynamicProgramming({
             {!!takeValue ? 
             <>
             <p>{"max: ("}</p>
-          <p className={`${isAddedBigger ? "text-red-400" : "text-green-300"}`}>{"\t"}dp[{step.i-1}][{step.w}] = {skipValue},</p>
-          <p className={`${isAddedBigger ? "text-green-300" : "text-red-400"}`}>{"\t"}dp[{step.i-1}][{step.w - weights[step.i-1]}] + {values[step.i-1]} = {takeValue},</p>
+          <p className={`ml-4 ${isNewValTaken ? "text-red-400" : "text-green-300"}`}>{"\t"}dp[{step.i-1}][{step.w}] = {skipValue},</p>
+          <p className={`ml-4 ${isNewValTaken ? "text-green-300" : "text-red-400"}`}>{"\t"}dp[{step.i-1}][{step.w - weights[step.i-1]}] + {values[step.i-1]} = {takeValue},</p>
           <p>{" )"}</p>
           </>
           :
           <>
-            <header className="text-amber-200 text-2xl">Value in prev row: </header>
+            <header className="text-amber-200 mt-4 text-2xl whitespace-nowrap">Value in prev row: </header>
 
-          <p>{"\t"}dp[{step.i-1}][{step.w}] = {skipValue},</p>
+          <p className="ml-4">{"\t"}dp[{step.i-1}][{step.w}] = {skipValue}</p>
+
           {/* <p className={`${isAddedBigger && "text-green-400"}`}>{"\t"}dp[{step.i-1}][{step.w - weights[step.i-1]}] + {values[step.i-1]} = {takeValue},</p> */}
           {/* <p>{" )"}</p> */}
+
           </>
           }
           
-          {takeValue && <p className="text-2xl">Then: <span className={`${isAddedBigger ? "text-red-400" : "text-green-400"}`}>{skipValue}</span> {skipValue < takeValue ? "<" : skipValue === takeValue ? "=" : ">"} <span className={`${isAddedBigger ? "text-green-400" : "text-red-400"}`}>{takeValue}</span> {"->"} take {isAddedBigger ? takeValue : skipValue} </p> }
+          {takeValue && <p className="text-2xl whitespace-nowrap">Then: <span className={`${isNewValTaken ? "text-red-400" : "text-green-300"}`}>{skipValue}</span> {skipValue < takeValue ? "<" : skipValue === takeValue ? "=" : ">"} <span className={`${isNewValTaken ? "text-green-300" : "text-red-400"}`}>{takeValue}</span> {"->"} take {isNewValTaken ? takeValue : skipValue} </p> }
+
+          <div className="w-full h-fit mt-4">
+          {status =="running"  ?
+            <RunAlgoButton  handleAlgorithms={() => controlAlgorithm("paused", type)} text= "Stop"></RunAlgoButton>:
+            <RunAlgoButton  handleAlgorithms={() => controlAlgorithm("running", type)} text="Continue"></RunAlgoButton>
+          }
+          </div>
+
           </>
         }
         </motion.div>
@@ -171,7 +184,7 @@ function DynamicProgramming({
           Dynamic Programming algorithm table:{" "}
         </label>
         <div className="absolute w-full h-6 top-0 left-0 flex items-center rounded-tl-2xl rounded-tr-2xl bg-neutral-500">
-          <button onClick={() => {setIsClosed(true); alert("yep!")}} className="rounded-tr-xl absolute right-1 h-4 w-10 flex justify-center items-center  bg-red-500/20 text-red-300 border border-red-500 ">
+          <button onClick={() => {setIsClosed(true);}} className="rounded-tr-xl absolute right-1 h-4 w-10 flex justify-center items-center  bg-red-500/20 text-red-300 border border-red-500 ">
 
           <svg className="pointer-events-none" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
   <line x1="18" y1="6" x2="6" y2="18"></line>
@@ -238,11 +251,12 @@ function DynamicProgramming({
               return (
                 <motion.div
                   variants={{pulse: {scale: [1.1, 1]}, static: {scale: 1}}}
+
                   transition={{
                   repeat: Infinity,
                   duration: 1,
-                  ease: "anticipate" }}
-                  animate = {algoFinished && step.i === ri - 1 && step.w === ci - 1 ? "pulse": "static"}
+                  ease: "easeInOut" }}
+                  animate = {( status == "finished") && step.i === ri - 1 && step.w === ci - 1 ? "pulse": "static"}
                   key={`${ri}-${ci}`}
                   className={`${steps[currentStep].i == ri - 1 && steps[currentStep].w == ci - 1 ? "border-green-300 border-6" : ""}  border flex items-center justify-center min-w-10 min-h-10  text-white`}
                 >
@@ -252,7 +266,6 @@ function DynamicProgramming({
             }),
           )}
         </section>
-
       </motion.div>
 }
     </>
